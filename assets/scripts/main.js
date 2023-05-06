4ec86aef11b176a11595ee7f2465c7ee5a8ef8c4
Ruchita Makwana <ruchitahm@gmail.com>
2023-05-05 22:53:40 -0400

    Initial Comment in Dev Branch

---
+++ assets/scripts/main.js
@@ -0,0 +1,308 @@
+
+{/* <script> */}
+var tablinks = document.getElementsByClassName("tablinks");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+// </script>
+
+{/* <script> */}
+var menu = document.getElementById("sidemenu");
+function openmenu(){
+    menu.style.right="0";
+    menu.classList.add('sidepanelopen');
+}
+function closemenu(){
+    menu.style.right="-200px";
+    menu.classList.remove('sidepanelopen');
+}
+function sectn(){
+    menu.style.right="-200px";
+}
+// </script>
+
+{/* <script> */}
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyqGH_aplWFeH-z_YjvVU42VmvYM8wdCqIhIEs9GzAEBIZT3p6x4WzQS0iYldh3A4zV/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById("msg");
+// form.addEventListener('submit', e => {
+// e.preventDefault()
+// fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//         msg.innerHTML = "Message send successfully!"
+//         setTimeout(function(){
+//             msg.innerHTML = ""
+//         }, 5000)
+//         e.preventDefault();
+//         form.reset();
+//         console.log('Success!', response)
+//     })
+//     .catch(error => console.error('Error!', error.message))
+// })
+// </script>
+
+{/* <script type="text/javascript"> */}
+
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('.navbarheader');
+    header.classList.toggle('sticky', window.scrollY>0);
+})
+
+const nameEl = document.querySelector('#name');
+const emailEl = document.querySelector('#email');
+const messageEl = document.querySelector('#message');
+const form1 = document.querySelector('#contactform');
+
+const checkName = () => {
+
+    let valid = false;
+    const min = 3,
+        max = 25;
+    const name = nameEl.value.trim();
+
+    if (!isRequired(name)) {
+        showError(nameEl, 'Name cannot be blank.');
+    } else if (!isBetween(name.length, min, max)) {
+        showError(nameEl, `Name must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(nameEl);
+        valid = true;
+    }
+    return valid;
+}
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const checkMessage = () => {
+
+    let valid = false;
+    const min = 5,
+        max = 25;
+    const message = messageEl.value.trim();
+
+    if (!isRequired(message)) {
+        showError(messageEl, 'Message cannot be blank.');
+    } else if (!isBetween(message.length, min, max)) {
+        showError(messageEl, `Message must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(messageEl);
+        valid = true;
+    }
+    return valid;
+}
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isRequired = value => value === '' ? false : true;
+
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+
+// const scriptURL = 'https://script.google.com/macros/s/AKfycbyqGH_aplWFeH-z_YjvVU42VmvYM8wdCqIhIEs9GzAEBIZT3p6x4WzQS0iYldh3A4zV/exec'
+// const form = document.forms['submit-to-google-sheet']
+// const msg = document.getElementById("msg");
+// form.addEventListener('submit', e => {
+// e.preventDefault()
+// fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//     .then(response => {
+//         msg.innerHTML = "Message send successfully!"
+//         setTimeout(function(){
+//             msg.innerHTML = ""
+//         }, 5000)
+//         // e.preventDefault();
+//         form.reset();
+//         console.log('Success!', response)
+//     })
+//     .catch(error => console.error('Error!', error.message))
+// })
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyqGH_aplWFeH-z_YjvVU42VmvYM8wdCqIhIEs9GzAEBIZT3p6x4WzQS0iYldh3A4zV/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+    // validate forms
+    let isNameValid = checkName(),
+        isEmailValid = checkEmail(),
+        isMessageValid = checkMessage();
+
+    let isFormValid = isNameValid &&
+        isEmailValid &&
+        isMessageValid;
+    // submit to the server if the form is valid
+    if (isFormValid) {
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message send successfully!"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            }, 2000)
+           
+            e.preventDefault();
+            form.reset();
+            console.log('Success!', response)
+        })
+        
+        .catch(error => console.error('Error!', error.message))
+        const elements = document.querySelectorAll('.success');
+    elements.forEach((element) => {
+      element.classList.remove('success');
+
+    });
+
+    document.querySelector('#name').disabled = true;
+        document.querySelector('#email').disabled = true;
+        document.querySelector('#message').disabled = true;
+        // document.querySelector('textarea').disabled = true;
+        setTimeout(function() {
+            // document.querySelector('textarea').disabled = false;
+            document.querySelector('#name').disabled = false;
+            document.querySelector('#email').disabled = false;
+            document.querySelector('#message').disabled = false;
+        }, 5000);
+    
+        // document.querySelectorAll('.success');
+        // document.querySelector(".form").find('.success').classList.remove("success");
+    }
+    // this.disabled = true;
+    // const aa = document.querySelectorAll('.success');;
+    // console.log('#@@@@@', aa)
+    // // remove the error class
+    // // aa.classList.remove('error');
+    // aa.classList.add('1111');
+    // let aa = document.querySelectorAll('.success');
+    //     console.log('#@@@@@', aa)
+    //     aa.classList.add("1111")
+});
+
+
+  
+
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'name':
+            checkName();
+            break;
+        case 'email':
+            checkEmail();
+            break;
+        case 'message':
+            checkMessage();
+            break;
+    }
+}));
+
+// document.querySelector('.submitbtn').addEventListener('click', function (e) {
+//     // submitform.onclick = () => {
+//         // document.querySelector('#name').disabled = true;
+//     //     const elements = document.querySelectorAll('.success');
+//     // elements.forEach((element) => {
+//     //   element.classList.remove('success');
+//     // });
+//     document.querySelector('#name').disabled = true;
+//     document.querySelector('#email').disabled = true;
+//     document.querySelector('#message').disabled = true;
+//     // document.querySelector('textarea').disabled = true;
+//     setTimeout(function() {
+//         // document.querySelector('textarea').disabled = false;
+//         document.querySelector('#name').disabled = false;
+//         document.querySelector('#email').disabled = false;
+//         document.querySelector('#message').disabled = false;
+//     }, 5000);
+//       })
+      
+// function validate(){
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("email").value;
+//     var message = document.getElementById("message").value;
+//     var error_message = document.getElementById("error_message");
+    
+//     error_message.style.padding = "10px";
+    
+//     var text;
+//     if(name.length < 2){
+//       text = "Please Enter valid Name";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(email.indexOf("@") == -1 || email.length < 4){
+//       text = "Please Enter valid Email";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     if(message.length <= 10){
+//       text = "Please Enter More Than 10 Characters";
+//       error_message.innerHTML = text;
+//       return false;
+//     }
+//     // alert("Form Submitted Successfully!");
+//     return true;
+//   }
+
+
+{/* </script> */}
